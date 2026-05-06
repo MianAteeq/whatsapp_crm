335f7a3f70b4814378f9c522772676985460888b
ateeqadrees <ateeqadrees@fissionmonster.com>
2026-05-06 16:55:28 +0500

    tag and contact api and seeder

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ContactController;
+use App\Http\Controllers\Api\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
