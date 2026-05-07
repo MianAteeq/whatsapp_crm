3229f51c3b4f441c5b9927cf696089f4af41b492
ateeqadrees <ateeqadrees@fissionmonster.com>
2026-05-07 21:50:11 +0500

    whatsapp settings connection

--- app/Http/Controllers/Api/WhatsappWebhookController.php
+++ app/Http/Controllers/Api/WhatsappWebhookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Contact;
 use App\Models\Conversation;
 use App\Models\Message;
 use App\Models\WhatsappSetting;
