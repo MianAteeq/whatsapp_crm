9340229e41dac1feffcbd3a44463f50cc3d67fcd
ateeqadrees <ateeqadrees@fissionmonster.com>
2026-05-07 21:53:25 +0500

    whatsapp settings connection

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Conversation extends Model
 {
+
+
+protected $guarded = [];
     public function contact()
     {
         return $this->belongsTo(Contact::class);
