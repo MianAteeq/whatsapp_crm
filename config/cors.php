6d015a107428b3f39672d61ac337e6c15d0f55fc
ateeqadrees <ateeqadrees@fissionmonster.com>
2026-05-20 17:43:30 +0500

    campaign

---
+++ config/cors.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    'paths' => [
+
+        'api/*',
+
+        'sanctum/csrf-cookie',
+
+    ],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+
+        'https://ais-dev-qcowfv4cefg67wa3butixx-757043192357.asia-east1.run.app',
+
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
